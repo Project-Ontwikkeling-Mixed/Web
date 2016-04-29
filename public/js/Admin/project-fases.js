5b7187a4ebede830086304627fa68c907d51aa11
Vincentdc94 <vincent-de-coen@hotmail.com>
2016-04-29 10:51:45 +0200

    fases kiezen in admin + alles in vue.js

--- public/js/Admin/project-fases.js
+++ public/js/Admin/project-fases.js
@@ -1,6 +1,11 @@
 new Vue({
   el: '#project-page',
 
+  data: {
+    project: '',
+    selectedFase: ''
+  },
+
   ready: function(){
     var id = $('#project-page').attr('data-id');
     this.fetchProject(id);
@@ -9,9 +14,16 @@ new Vue({
   methods: {
     fetchProject: function(id){
       this.$http.get('/json/project/' + id, function(project){
-        console.log(project);
         this.$set('project', project);
       })
+    },
+
+    tabFase: function(event){
+      faseId = event.target.id;
+
+      project = this.$data.project;
+
+      this.$set('selectedFase', project.fases[faseId - 1]);
     }
   }
 })
